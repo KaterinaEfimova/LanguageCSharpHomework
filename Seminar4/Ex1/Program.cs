e0f5b5a42f94d3c7230ed5e11962f3342c86d41e
Katerina Efimova <efimovaen2016@mail.ru>
2023-08-06 19:39:52 +0500

    Seminar 4

---
+++ Seminar4/Ex1/Program.cs
@@ -0,0 +1,24 @@
+﻿// Напишите цикл, который принимает на вход два числа (A и B)
+//  и возводит число A в натуральную степень B.
+// 3, 5 -> 243 (3⁵)
+// 2, 4 -> 16
+
+Console.Write("Введите число - основание степени : ");
+int a = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите число - показатель степени : ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+int  Degree()
+{
+    int result = 1;
+    int count = 1;
+    while (count <= n)
+    {
+        result = result * a;
+        count++; 
+    }
+    return result;
+}
+
+Console.Write(Degree());
---
+++ Seminar4/Ex2/Program.cs
@@ -0,0 +1,24 @@
+﻿// Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+// 452 -> 11
+// 82 -> 10
+// 9012 -> 12
+
+
+Console.Write("Введите число: ");
+int num1 = Convert.ToInt32(Console.ReadLine());
+string num2 = Convert.ToString(num1);
+
+int Sum()
+{
+    int res = 0;
+    int size = num2.Length;
+    for (int i = 0; i < size; i++)
+    {
+        res += num1 % 10;
+        num1 /= 10;
+    }
+    return res;
+}
+
+Console.WriteLine();
+Console.WriteLine(Sum());

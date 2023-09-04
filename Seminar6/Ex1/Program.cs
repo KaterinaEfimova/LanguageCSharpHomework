1f92201cebf46731c5670a5c866d84191bdabc2a
Katerina Efimova <efimovaen2016@mail.ru>
2023-09-04 07:28:37 +0300

    Homework 6

---
+++ Seminar6/Ex1/Program.cs
@@ -0,0 +1,48 @@
+﻿//Пользователь вводит с клавиатуры M чисел.
+//Посчитайте, сколько чисел больше 0 ввёл пользователь.
+
+
+Console.WriteLine("Введите количество чисел");
+int len = Convert.ToInt32(Console.ReadLine());
+
+int[] array = new int [len];
+
+void AddArray()
+{
+   for (int i = 0; i < len; i++)
+   {
+    Console.WriteLine($"Введите {i+1} число");
+    array[i] = Convert.ToInt32(Console.ReadLine());    
+   }
+   }
+
+void PrintArray()
+{
+    Console.Write ("Введены числа:");
+    for (int i = 0; i < array.Length; i++)
+    {        
+        Console.Write(array[i]+" ");
+    }
+}
+
+void PositivNumber()
+
+{
+    int count = 0;
+   
+    for (int i = 0; i < len; i++)
+    {
+        if (array[i]>0)
+    {       
+        count= count+1;     
+    }
+    }
+    System.Console.WriteLine("");
+    System.Console.Write("Количество положительных чисел:");
+    System.Console.Write(count);
+}
+
+AddArray();
+PrintArray();
+PositivNumber();
+
---
+++ Seminar6/Ex2/Program.cs
@@ -0,0 +1,26 @@
+﻿//  Напишите программу, которая найдёт точку пересечения двух прямых,
+//  заданных уравнениями y = k1 * x + b1, y = k2 * x + b2; значения
+//  b1, k1, b2 и k2 задаются пользователем.
+
+Console.WriteLine("Введите k1");
+float k1 = Convert.ToSingle(Console.ReadLine());
+
+Console.WriteLine("Введите b1");
+float b1 = Convert.ToSingle(Console.ReadLine());
+
+Console.WriteLine("Введите k2");
+float k2 = Convert.ToSingle(Console.ReadLine());
+
+Console.WriteLine("Введите b2");
+float b2 = Convert.ToSingle(Console.ReadLine());
+
+if (k1==k2)
+{
+    Console.WriteLine("Прямые параллельны, пересечений нет");
+}
+else
+{
+    float x = (b2-b1)/(k1-k2);
+    float y = k1*x-b1;
+    Console.WriteLine($"Прямые пересекаются в точке ({x};{y})");
+}
